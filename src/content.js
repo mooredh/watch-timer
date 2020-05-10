c37e81f15085d4ce6ea4b318df12708ae9bb8250
mooredh <mooredh99@gmail.com>
2020-05-10 22:39:02 +0100

    Fixed minor bugs

--- src/content.js
+++ src/content.js
@@ -1,9 +1,3 @@
-let data = {}
-
-chrome.storage.sync.get("watch-time", function(items){
-    if (items["watch-time"]) data = items['watch-time']
-});
-
 class BaseTimer {
     constructor(hours = 0, mins = 0, secs = 0) {
         this.hours = hours
@@ -44,6 +38,13 @@ class BaseTimer {
     getHours = () => this.hours
 }
 
+let data = {}
+let date = new Date().toDateString()
+
+data[date] = {}
+
+let timer = new BaseTimer()
+
 const regExp = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/+/gm
 let url;
 
@@ -56,24 +57,38 @@ switch (regExp.test(window.location.href)) {
         break;
 }
 
-let timer = data[url] || new BaseTimer()
 let seconds = 0
 
 function cb() {
     seconds++
     if (seconds === 60) {
         timer.addMinute()
-        data[url] = timer
+        data[date][url] = timer
         chrome.storage.sync.set({ "watch-time": data });
         seconds = 0
     }
 }
 
-let interval = setInterval(cb, 1000)
-if (document.hidden) clearInterval(interval)
-document.addEventListener('visibilitychange', function(){
-    if (document.hidden) clearInterval(interval)
-    else {
-        interval = setInterval(cb, 1000)
+chrome.storage.sync.get("watch-time", function(items){
+    if (items["watch-time"]) {
+        data = items['watch-time']
+        if (data[date] === undefined) {
+            data[date] = {}
+        }
+
+        if (data[date][url]) {
+            const { hours, mins } = data[date][url]
+            timer = new BaseTimer(hours, mins)
+        }
     }
-})
+
+    let interval = setInterval(cb, 1000)
+
+    if (document.hidden) clearInterval(interval)
+    document.addEventListener('visibilitychange', function(){
+        if (document.hidden) clearInterval(interval)
+        else {
+            interval = setInterval(cb, 1000)
+        }
+    })
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 $(function() {
     let data = {}
     chrome.storage.sync.get("watch-time", function(items){
+        let date = new Date().toDateString()
         data = items['watch-time']
-        if (data.youtube) {
-            $('#youtube-timer').text(`${data.youtube.hours}h ${data.youtube.mins}m`)
-        }
-        if (data.netflix) {
-            $('#netflix-timer').text(`${data.netflix.hours}h ${data.netflix.mins}m`)
+        if (data) {
+            if (data[date]) {
+                if (data[date].youtube) {
+                    $('#youtube-timer').text(`${data[date].youtube.hours}h ${data[date].youtube.mins}m`)
+                }
+                if (data[date].netflix) {
+                    $('#netflix-timer').text(`${data[date].netflix.hours}h ${data[date].netflix.mins}m`)
+                }
+            }
         }
     });
     let active = 'today'
@@ -15,5 +20,35 @@ $(function() {
         $('.tab-items div').removeClass('active')
         $(this).addClass('active');
         active = $(this).text().toLowerCase()
+        let day;
+        if (active === 'this week') {
+            let youtube = { hours: 0, mins: 0 }
+            let netflix = { hours: 0, mins: 0 }
+            let tmp = new Date()
+
+            for (let i = 0; i < 7; i++) {
+                day = new Date(tmp.setDate(tmp.getDate() - i)).toDateString()
+
+                if (data[day].youtube) {
+                    youtube.hours += data[day].youtube.hours
+                    youtube.mins += data[day].youtube.mins
+                }
+                if (data[day].netflix) {
+                    netflix.hours += data[day].netflix.hours
+                    netflix.mins += data[day].netflix.mins
+                }
+                if (day.substring(0, 3) === 'Sun') break;
+            };
+            $('#youtube-timer').text(`${youtube.hours}h ${youtube.mins}m`)
+            $('#netflix-timer').text(`${netflix.hours}h ${netflix.mins}m`)
+        } else {
+            day = new Date().toDateString()
+            if (data[day].youtube) {
+                $('#youtube-timer').text(`${data[day].youtube.hours}h ${data[day].youtube.mins}m`)
+            }
+            if (data[day].netflix) {
+                $('#netflix-timer').text(`${data[day].netflix.hours}h ${data[day].netflix.mins}m`)
+            }
+        }
     })
 })
