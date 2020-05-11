c13a9e913a0b698af7b909bd8f213771273e1c85
mooredh <mooredh99@gmail.com>
2020-05-11 09:59:53 +0100

    removed jquery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-$(function() {
+window.onload = function() {
     let data = {}
     chrome.storage.sync.get("watch-time", function(items){
         let date = new Date().toDateString()
@@ -6,60 +6,64 @@ $(function() {
         if (data) {
             if (data[date]) {
                 if (data[date].youtube) {
-                    $('#youtube-timer').text(`${data[date].youtube.hours}h ${data[date].youtube.mins}m`)
+                    document.getElementById('youtube-timer').innerHTML = `${data[date].youtube.hours}h ${data[date].youtube.mins}m`
                 }
                 if (data[date].netflix) {
-                    $('#netflix-timer').text(`${data[date].netflix.hours}h ${data[date].netflix.mins}m`)
+                    document.getElementById('netflix-timer').innerHTML = `${data[date].netflix.hours}h ${data[date].netflix.mins}m`
                 }
             }
         }
     });
     let active = 'today'
 
-    $('.tab-items div').click(function() {
-        $('.tab-items div').removeClass('active')
-        $(this).addClass('active');
-        active = $(this).text().toLowerCase()
-        let day;
-        if (active === 'this week') {
-            let youtube = { hours: 0, mins: 0 }
-            let netflix = { hours: 0, mins: 0 }
-            let tmp = new Date()
+    document.querySelectorAll('.tab-items div').forEach(function(div) {
+        div.addEventListener('click', function() {
+            document.querySelectorAll('.tab-items div').forEach(function(elem) {
+                elem.classList.remove('active')
+            })
+            div.classList.add('active')
+            active = div.innerHTML.toLowerCase()
+            let day;
+            if (active === 'this week') {
+                let youtube = { hours: 0, mins: 0 }
+                let netflix = { hours: 0, mins: 0 }
+                let tmp = new Date()
 
-            for (let i = 0; i < 7; i++) {
-                day = new Date(tmp.setDate(tmp.getDate() - i)).toDateString()
+                for (let i = 0; i < 7; i++) {
+                    day = new Date(tmp.setDate(tmp.getDate() - i)).toDateString()
 
+                    if (data[day]) {
+                        if (data[day].youtube) {
+                            youtube.hours += data[day].youtube.hours
+                            youtube.mins += data[day].youtube.mins
+                        }
+                        if (data[day].netflix) {
+                            netflix.hours += data[day].netflix.hours
+                            netflix.mins += data[day].netflix.mins
+                        }
+                    }
+                    if (day.substring(0, 3) === 'Sun') break;
+                };
+                document.getElementById('youtube-timer').innerHTML = `${youtube.hours}h ${youtube.mins}m`
+                document.getElementById('netflix-timer').innerHTML = `${netflix.hours}h ${netflix.mins}m`
+            } else {
+                day = new Date().toDateString()
                 if (data[day]) {
                     if (data[day].youtube) {
-                        youtube.hours += data[day].youtube.hours
-                        youtube.mins += data[day].youtube.mins
+                        document.getElementById('youtube-timer').innerHTML = `${data[day].youtube.hours}h ${data[day].youtube.mins}m`
+                    } else {
+                        document.getElementById('youtube-timer').innerHTML = '0h 0m'
                     }
                     if (data[day].netflix) {
-                        netflix.hours += data[day].netflix.hours
-                        netflix.mins += data[day].netflix.mins
+                        document.getElementById('netflix-timer').innerHTML = `${data[day].netflix.hours}h ${data[day].netflix.mins}m`
+                    } else {
+                        document.getElementById('netflix-timer').innerHTML = '0h 0m'
                     }
-                }
-                if (day.substring(0, 3) === 'Sun') break;
-            };
-            $('#youtube-timer').text(`${youtube.hours}h ${youtube.mins}m`)
-            $('#netflix-timer').text(`${netflix.hours}h ${netflix.mins}m`)
-        } else {
-            day = new Date().toDateString()
-            if (data[day]) {
-                if (data[day].youtube) {
-                    $('#youtube-timer').text(`${data[day].youtube.hours}h ${data[day].youtube.mins}m`)
                 } else {
-                    $('#youtube-timer').text('0h 0m')
+                    document.getElementById('youtube-timer').innerHTML = '0h 0m'
+                    document.getElementById('netflix-timer').innerHTML = '0h 0m'
                 }
-                if (data[day].netflix) {
-                    $('#netflix-timer').text(`${data[day].netflix.hours}h ${data[day].netflix.mins}m`)
-                } else {
-                    $('#netflix-timer').text('0h 0m')
-                }
-            } else {
-                $('#netflix-timer').text('0h 0m')
-                $('#youtube-timer').text('0h 0m')
             }
-        }
+        })
     })
-})
+}
